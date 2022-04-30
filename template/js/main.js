c74627084e8eeb50accafd44373005760265e5b1
shun198 <shunhiroseluvmri@gmail.com>
2022-04-30 09:10:11 +0900

    create vue template

--- template/js/main.js
+++ template/js/main.js
@@ -1,12 +1,7 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello Vue!',
-  }),
-  methods: {
-    clickHandler: function(event) {
-      this.message = this.message.split('').reverse().join('');
-    }
-  }
+    message: 'Hello Vue',
+  })
 })
 
 app.mount('#app')
